a1373ee0c0fd762e5ba06d1b89f805a173f28963
Alquipo <alquiponeto@outlook.com.br>
2021-05-27 14:06:45 -0300

    feat: stripe intents payment router

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,12 +1,56 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 module.exports = {
-  createPaymentIntent: () => {
-    return 'hejf'
+  createPaymentIntent: async (ctx) => {
+    const {cart} = ctx.request.body
+
+    let games = []
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id
+        })
+
+        if(validatedGame){
+          games.push(validatedGame)
+        }
+      })
+    )
+
+    if(!games.length) {
+      ctx.response.status = 404
+      return {
+        error: "No valid games found!"
+      }
+    }
+
+    const total = games.reduce((acc, game) => {
+      return acc + game.price
+    }, 0)
+
+    if(total === 0) {
+      return {
+        freeGames: true
+      }
+    }
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total * 100,
+        currency: "usd",
+        metadata: {integration_check: "accept_a_payment"}
+      })
+
+      return paymentIntent
+    } catch (err) {
+      return {
+        error: err.raw.message
+      }
+    }
+
+
   }
 };

1ebe78525537154cd4b215ae6f8577fa805b4340
Alquipo <alquiponeto@outlook.com.br>
2021-05-28 09:30:38 -0300

    fix: save orders BDs

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -33,7 +33,7 @@ module.exports = {
 
     try {
       const paymentIntent = await stripe.paymentIntents.create({
-        amount: total * 100,
+        amount: total,
         currency: "usd",
         metadata: { cart: JSON.stringify(cartGamesIds)}
       })
@@ -71,12 +71,22 @@ module.exports = {
     const total_in_cents = await strapi.config.functions.cart.total(games)
 
 
+    //pegar os dados do front end os valore payment Method
+    let paymentInfo;
+    if(total_in_cents !== 0) {
+      try {
+        paymentInfo = await stripe.paymentMethods.retrieve(paymentMethod)
+      } catch (err) {
+        ctx.response.status = 402
+        return {error: err.message}
+      }
+    }
     //salvar no banco
     const entry = {
       total_in_cents,
       payment_intent_id: paymentIntentId,
-      card_brand: null,
-      card_last4: null,
+      card_brand: paymentInfo?.card?.brand,
+      card_last4: paymentInfo?.card?.last4,
       user: userInfo,
       games
     }

a6145305c162b61f74cc7aa342259413d275f5f3
Alquipo <alquiponeto@outlook.com.br>
2021-06-15 18:23:34 -0300

    fix: database config

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'wonGames'),
-        username: env('DATABASE_USERNAME', 'wonGames'),
-        password: env('DATABASE_PASSWORD', 'wonGames123'),
+        database: env('DATABASE_NAME', 'wongames'),
+        username: env('DATABASE_USERNAME', 'wongames'),
+        password: env('DATABASE_PASSWORD', 'wongames123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
